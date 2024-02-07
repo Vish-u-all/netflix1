5d528b41df7572a023a7c2b65c63d66f3b71137f
Vishal <66079506+Vish-u-all@users.noreply.github.com>
2024-02-08 04:32:04 +0530

    netflix1
    Netflix Clone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.scss';
+import Header from './components/Header.js';
+import HomeBanner from "./components/HomeBanner";
+import Login from "./components/Login";
+import Banner from "./components/Banner";
+import List from "./components/List";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Default tag is BrowserRouter and here we declared the tag as Router
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+{/*React.Fragment is used to avoid extra space unline div tag and it is also faster*/}
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <React.Fragment>
+              <Header/>
+              <HomeBanner/>
+            </React.Fragment>
+          }/>
+          {/* <Route path="/login" element={
+            <React.Fragment>
+              <Header/>
+              <Login/>
+            </React.Fragment>
+          }/> */}
+          <Route path="/register" element={
+            <React.Fragment>
+              <Header/>
+              <Login/>
+            </React.Fragment>
+          }/>
+          <Route path="/dashboard" element={
+            <React.Fragment>
+              <Header/>
+              <Banner/>
+              <List title="Netflix Originals" param="originals"/>
+              <List title="Trending Now" param="trending"/>
+              <List title="Now Playing" param="now_playing"/>
+              <List title="popular" param="popular"/>
+              <List title="Top Rated" param="top_rated"/>
+              <List title="Upcoming" param="upcoming"/>
+            </React.Fragment>
+          }/>
+        </Routes>
+      </Router>
+    </React.Fragment>
   );
 }
 
